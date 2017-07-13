8c915f19b064c58d62f348daaddd646ae2650cbd
Andrew Liao <andrew.sf.liao@gmail.com>
2017-07-12 19:42:43 -0700

    Adding the gal to literes calculation

--- src/pack1/Apreview.java
+++ src/pack1/Apreview.java
@@ -1,32 +1,11 @@
 package pack1;
 
-import math.Math;
-
 public class Apreview {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		
-		Person son1 = new Person("liao", "andrew", "abcde", "111-22-3333");
-		System.out.println(son1);
-		
-		Person son2 = new Person();
-		son2.setFirstName("Brian");
-		son2.setLastName("Liao");
-		System.out.println(son2);
-		System.out.println(son2.getId());
-		
-		son1 = son2;
-		
-		System.out.println(son1);
-		
-		new Person();
-		new Person();
-		
-		int x = 2;
-		double y = 0.22;
-		
-		System.out.println(Math.PI);
-		
-		System.out.println(son1.getCounter());
+		//this is a gal to liters calculator
+		double gal, liters;
+		gal = 10;
+		liters = gal * 3.7854;
+		System.out.println(gal + " gallons is " + liters + " liters.");
 	}
 }
