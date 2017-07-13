146724431fd129c8ac1301567487dadc2b6e6564
Andrew Liao <andrew.sf.liao@gmail.com>
2017-07-12 20:00:54 -0700

    moving contents to another file

--- src/pack1/Apreview.java
+++ src/pack1/Apreview.java
@@ -2,10 +2,6 @@ package pack1;
 
 public class Apreview {
 	public static void main(String[] args) {
-		//this is a gal to liters calculator
-		double gal, liters;
-		gal = 10;
-		liters = gal * 3.7854;
-		System.out.println(gal + " gallons is " + liters + " liters.");
+		
 	}
 }
