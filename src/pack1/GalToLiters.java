b50e68463f64de3cd02287628ce558f1baa48a11
Andrew Liao <andrew.sf.liao@gmail.com>
2017-07-12 20:02:58 -0700

    creating 1-1 exercise

---
+++ src/pack1/GalToLiters.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class GalToLiters {
+	public static void main(String[] args) {
+		//this is a gal to liters calculator
+		double gal, liters;
+		gal = 10;
+		liters = gal * 3.7854;
+		System.out.println(gal + " gallons is " + liters + " liters.");
+	}
+}
