99533bd6537d795537dcab89a24bb0ff0060555f
Andrew Liao <andrew.sf.liao@gmail.com>
2017-07-23 14:13:25 -0700

    sound.java

---
+++ src/pack1/Sound.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class Sound {
+	public static void main(String[] args) {
+		double dist;
+		
+		dist = 7.2 * 1100;
+		
+		System.out.println("The lighting is " + dist + " feet away.");
+		
+	}
+}
